bbf4c9bcceb50fe492a6501b28e696302b902c80
Bill Wang <ozbillwang@gmail.com>
2014-11-06 12:20:29 +1100

    Update aws-ses.sh

--- awscli/aws-ses.sh
+++ awscli/aws-ses.sh
@@ -1,9 +1,8 @@
-# Get the bounces rate (less than 1 hour) from AWS SES services.
+#! /usr/bin/env bash
 # You should have aws config ready for key,security and region.
 
-# Get alert if bounces mail rate is higher than 5%.
-
+# Get alert if bounces mail rate is higher than 5% in one hour.
 aws ses get-send-statistics --output text |awk '{gsub(/[-:]/,"");gsub(/[TZ]/," ",$NF);}1' |sort -k6,7  |tail -4|awk '{bounces+=$2;total+=$4}END{if (bounces/total>0.05) print "high error rate (>5%)"}'
 
-# Get alert when, say, 80% of  sending quota has been reached.
+# Get alert when, say, 80% of sending quota has been reached.
 aws ses get-send-quota --output text |awk '{if ($3/$1>0.8) print "80% of sending quota has been reached"}'
