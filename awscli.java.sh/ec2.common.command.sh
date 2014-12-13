bcb66b05939b2f4378f9ffc462f07de7d77adc15
Bill W <SydOps@users.noreply.github.com>
2014-12-14 10:33:25 +1100

    Update ec2.common.command.sh

--- awscli.java.sh/ec2.common.command.sh
+++ awscli.java.sh/ec2.common.command.sh
@@ -1,4 +1,8 @@
+# list the running or terminal instances (stopped instances are not in list)
+ec2-describe-instances --region us-west-2 |awk '/INSTANCE/{print $2}'
 
-ec2-describe-instances --region us-west-2 |grep INSTANCE
+i-0744b709
+i-b16a9bbf
 
+# Create AMI image base on one of instance ID i-b16a9bbf
 ec2-create-image i-b16a9bbf --name "Standard Web Server" --region us-west-2 --description "Standard web server AMI" -b "/dev/sdc=ephemeral0"
