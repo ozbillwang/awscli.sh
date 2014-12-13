e2acf4278d6f5e528986949202f2e5b2fb9bc9c1
Bill W <SydOps@users.noreply.github.com>
2014-12-14 10:55:11 +1100

    Update ec2.common.command.sh

--- awscli.java.sh/ec2.common.command.sh
+++ awscli.java.sh/ec2.common.command.sh
@@ -6,3 +6,8 @@ i-b16a9bbf
 
 # Create AMI image base on one of instance ID i-b16a9bbf
 ec2-create-image i-b16a9bbf --name "Standard Web Server" --region us-west-2 --description "Standard web server AMI" -b "/dev/sdc=ephemeral0"
+
+ec2stop i-0744b709 --region us-west-2
+
+describe default Amazon Linux image 
+ec2-describe-images ami-b5a7ea85 --region us-west-2
