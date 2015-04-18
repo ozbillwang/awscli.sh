a13bac63f254a17185cb970d2cefd585e1cc207b
Bill W <SydOps@users.noreply.github.com>
2015-04-19 08:23:04 +1000

    Collect the command from latest aws slideshare.
    http://www.slideshare.net/AmazonWebServices/deep-dive-advanced-usage-of-the-aws-cli?utm_source=slideshow&utm_medium=ssemail&utm_campaign=upload_digest

---
+++ awscli/create-new-user.sh
@@ -0,0 +1,8 @@
+aws iam create-user --user-name summit-user
+credentials=$(aws iam create-access-key --user-name summit-user \
+  --query 'AccessKey.[AccessKeyId,SecretAccessKey]' \
+  --output text)
+access_key_id=$(echo $credentials |cut -d' '  -f1)
+secret_access_key=$(echo $credentials |cut -d' '  -f2)
+aws configure set profile.summit.aws_access_key_id "$access_key_id"
+aws configure set profile.summit.secret_access_key "$secret_access_key"
