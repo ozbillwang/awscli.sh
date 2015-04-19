3ee698be2bee3a9b8bb1b8ab601a0760cdf5de1e
Bill W <SydOps@users.noreply.github.com>
2015-04-19 21:09:14 +1000

    Update create-new-user.sh

--- awscli/create-new-user.sh
+++ awscli/create-new-user.sh
@@ -22,4 +22,14 @@ aws configure set profile.summit.aws_secret_access_key "$secret_access_key"
 aws iam list-users --query Users[0].[UserName,Path,UserId]
 aws iam list-users --query Users[0].[UserName,Path,UserId] --profile prod
 
-aws ec2 run-instances --image-id ami-fd4724c7 --instance-type t2.micro
+instance_id=$(aws ec2 run-instances --image-id ami-fd4724c7 --instance-type t2.micro --query Instances[].InstanceId --output text)
+instance_state=$(aws ec2 describe-instances --instance-ids $instance_id \
+    --query 'Reservations[].Instances[].State.Name' \
+    --output text)
+while [ "$instance_state" != "running" ]
+do
+    sleep 1
+    instance_state=$(aws ec2 describe-instances  --instance-ids $instance_id \
+        --query 'Reservations[].Instances[].State.Name' \
+        --output text)
+done
